a85783cd009120aa50afbe242201318cd538dbea
bragamateus <braga.mateus16@gmail.com>
2020-01-16 17:15:33 -0300

    Criando o backend da aplicação. Primeiros conceitos do express e arquitetura REST

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros
+
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: req.params (Identificar um recurso na alteração ou remeção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body)
+    return response.json({message: "Hello World"})
+})
+
+app.listen(3333)
