7bfdd03d87113c42d6df259bf669f7b0b27e5814
bragamateus <braga.mateus16@gmail.com>
2020-01-17 00:38:28 -0300

    cadastrando dev no banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express()
 
+mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-9e9av.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 app.use(express.json())
+app.use(routes)
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
@@ -12,9 +20,5 @@ app.use(express.json())
 // Route Params: req.params (Identificar um recurso na alteração ou remeção)
 // Body: request.body (Dados para criação ou alteração de um registro)
 
-app.post('/users', (request, response) => {
-    console.log(request.body)
-    return response.json({message: "Hello World"})
-})
 
 app.listen(3333)
