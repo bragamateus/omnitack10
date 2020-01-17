86594c607bbef6ed3bf08f2064613bf0dca93984
bragamateus <braga.mateus16@gmail.com>
2020-01-17 12:43:17 -0300

    Pegando as coordenadas do dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,5 @@ mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-9e9av.mongodb.n
 app.use(express.json())
 app.use(routes)
 
-// Métodos HTTP: GET, POST, PUT, DELETE
-
-// Tipos de parâmetros
-
-// Query Params: req.query (Filtros, ordenação, paginação, ...)
-// Route Params: req.params (Identificar um recurso na alteração ou remeção)
-// Body: request.body (Dados para criação ou alteração de um registro)
-
 
 app.listen(3333)
