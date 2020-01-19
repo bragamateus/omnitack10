9d5d5d256b4a9c8322cfea8a5cca7e8d8e0b1faf
bragamateus <braga.mateus16@gmail.com>
2020-01-19 16:42:13 -0300

    Terminando layout da página principal

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './main.css'
+
 
 
 function App() {
@@ -13,23 +15,23 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="username_github" required />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
               <input name="latitude" id="latitude" required />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
               <input name="longitude" id="longitude" required />
             </div>
@@ -38,8 +40,58 @@ function App() {
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main>
 
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/16947985?v=4" alt="Mateus Braga" />
+              <div className="user-info">
+                <strong>Mateus Braga</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>  
+            <p>Bacharelado em Ciência da Computação - UniCEUB</p>
+            <a href="https://github.com/bragamateus">Acessar perfil no Github</a>   
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/16947985?v=4" alt="Mateus Braga" />
+              <div className="user-info">
+                <strong>Mateus Braga</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>  
+            <p>Bacharelado em Ciência da Computação - UniCEUB</p>
+            <a href="https://github.com/bragamateus" target="blank">Acessar perfil no Github</a>   
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/16947985?v=4" alt="Mateus Braga" />
+              <div className="user-info">
+                <strong>Mateus Braga</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Bacharelado em Ciência da Computação - UniCEUB</p>
+            <a href="https://github.com/bragamateus">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/16947985?v=4" alt="Mateus Braga" />
+              <div className="user-info">
+                <strong>Mateus Braga</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Bacharelado em Ciência da Computação - UniCEUB</p>
+            <a href="https://github.com/bragamateus">Acessar perfil no Github</a>
+          </li>
+
+        </ul>
       </main>
     </div>
   )
